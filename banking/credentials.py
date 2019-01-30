a93bce2c46b08e508baa5043c493657c65f6d6b0
nidzo <16163816+nidzov@users.noreply.github.com>
2019-01-30 11:14:40 +0100

    commented sp banking out

--- banking/credentials.py
+++ banking/credentials.py
@@ -53,35 +53,35 @@ class Credentials:
                 if exco.errno != errno.EEXIST:
                     raise
 
-    def prompt_sp_credentials(self):
-        try:
-            if click.confirm(' Wollen Sie die Anmeldedaten eingeben?', abort=True):
-                username = click.prompt(' Konto Name')
-                password = click.prompt(' Passwort', hide_input=True)
+    # def prompt_sp_credentials(self):
+    #     try:
+    #         if click.confirm(' Wollen Sie die Anmeldedaten eingeben?', abort=True):
+    #             username = click.prompt(' Konto Name')
+    #             password = click.prompt(' Passwort', hide_input=True)
 
-                data = {
-                    'username': str(base64.b64encode(bytes(username, 'UTF-8')), 'UTF-8'),
-                }
+    #             data = {
+    #                 'username': str(base64.b64encode(bytes(username, 'UTF-8')), 'UTF-8'),
+    #             }
 
-                with open(self.sp_credentialFile, 'w') as outfile:
-                    json.dump(data, outfile)
+    #             with open(self.sp_credentialFile, 'w') as outfile:
+    #                 json.dump(data, outfile)
 
-        except OSError as exc:
-            if exc.errno != errno.EEXIST:
-                raise
+    #     except OSError as exc:
+    #         if exc.errno != errno.EEXIST:
+    #             raise
 
-    def get_sp_credentials(self):
-        if Path(self.sp_credentialFile).is_file():
-            try:
-                with open(self.sp_credentialFile) as creds:
-                    data = json.load(creds)
-                    username = str(base64.b64decode(data['username']), 'UTF-8')
+    # def get_sp_credentials(self):
+    #     if Path(self.sp_credentialFile).is_file():
+    #         try:
+    #             with open(self.sp_credentialFile) as creds:
+    #                 data = json.load(creds)
+    #                 username = str(base64.b64decode(data['username']), 'UTF-8')
 
-                    encodedData = {
-                        'username': username
-                    }
+    #                 encodedData = {
+    #                     'username': username
+    #                 }
 
-                    return encodedData
-            except OSError as exco:
-                if exco.errno != errno.EEXIST:
-                    raise
+    #                 return encodedData
+    #         except OSError as exco:
+    #             if exco.errno != errno.EEXIST:
+    #                 raise
--- banking/navigation.py
+++ banking/navigation.py
@@ -13,7 +13,7 @@ from banking.credentials import Credentials
 
 class Navigation:
 
-    SP_LOGIN_URL = "https://www.sparkasse-ulm.de/de/home.html"
+    # SP_LOGIN_URL = "https://www.sparkasse-ulm.de/de/home.html"
     DB_LOGIN_URL = "https://meine.deutsche-bank.de/trxm/db/init.do"
 
     def navigate_db_page(self):
@@ -72,43 +72,43 @@ class Navigation:
         page = browser.get_current_page()
         return page
 
-    def navigate_sp_page(self):
-        browser = mechanicalsoup.StatefulBrowser(
-            soup_config={'features': 'lxml'},
-            raise_on_404=True
-        )
-        result = browser.open(self.SP_LOGIN_URL)
+    # def navigate_sp_page(self):
+    #     browser = mechanicalsoup.StatefulBrowser(
+    #         soup_config={'features': 'lxml'},
+    #         raise_on_404=True
+    #     )
+    #     result = browser.open(self.SP_LOGIN_URL)
 
-        if result.status_code != 200:
-            click.echo(' Überprüfen Sie ihre Internetverbindung.')
-            exit()
+    #     if result.status_code != 200:
+    #         click.echo(' Überprüfen Sie ihre Internetverbindung.')
+    #         exit()
 
-        browser.select_form('form[class="header-login"]')
-        page = browser.get_current_page()
-        user_input = page.find_all(
-            'input', attrs={'value': '', 'maxlength': '16'})
-        password_input = page.find_all(
-            'input', attrs={'value': '', 'maxlength': '5'})
+    #     browser.select_form('form[class="header-login"]')
+    #     page = browser.get_current_page()
+    #     user_input = page.find_all(
+    #         'input', attrs={'value': '', 'maxlength': '16'})
+    #     password_input = page.find_all(
+    #         'input', attrs={'value': '', 'maxlength': '5'})
 
-        user_attribute_name = user_input[0]['name']
-        password_attribute_name = password_input[0]['name']
+    #     user_attribute_name = user_input[0]['name']
+    #     password_attribute_name = password_input[0]['name']
 
-        creds = Credentials().get_sp_credentials()
-        username = creds['username']
-        password = click.prompt(' PIN', hide_input=True)
+    #     creds = Credentials().get_sp_credentials()
+    #     username = creds['username']
+    #     password = click.prompt(' PIN', hide_input=True)
 
-        browser[user_attribute_name] = username
-        browser[password_attribute_name] = password
-        result = browser.submit_selected()
+    #     browser[user_attribute_name] = username
+    #     browser[password_attribute_name] = password
+    #     result = browser.submit_selected()
 
-        if result.status_code != 200:
-            click.echo(' Anmelde Daten sind falsch!')
-            exit()
+    #     if result.status_code != 200:
+    #         click.echo(' Anmelde Daten sind falsch!')
+    #         exit()
 
-        page = browser.get_current_page()
+    #     page = browser.get_current_page()
 
-        link = page.find('a', text='Umsätze abrufen')
-        result = browser.follow_link(link)
+    #     link = page.find('a', text='Umsätze abrufen')
+    #     result = browser.follow_link(link)
 
-        page = browser.get_current_page()
-        return page
+    #     page = browser.get_current_page()
+    #     return page
